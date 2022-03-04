0cae0bb02dae1a9447f2f7a97b157d9485f86eeb
Marco Casula <marco.casula@gmail.com>
2022-03-04 14:40:10 +0100

    impl FormData

--- src/net.ts
+++ src/net.ts
@@ -1,5 +1,22 @@
+import FormData from 'form-data'
+import {createReadStream} from 'fs'
 import http from 'http'
 import https from 'https'
+
+export const optionFactory = (
+  urlObject: URL,
+  apiKey: string,
+  headers: {[key: string]: string | number}
+): https.RequestOptions => ({
+  hostname: urlObject.hostname,
+  path: urlObject.pathname,
+  port: urlObject.port,
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+    ...headers
+  }
+})
 
 export const post = async <T>(
   url: string,
@@ -11,17 +28,10 @@ export const post = async <T>(
 
     const request = urlObject.protocol === 'https:' ? https : http
 
-    const options = {
-      hostname: urlObject.hostname,
-      path: urlObject.pathname,
-      port: urlObject.port,
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
-        'Content-Length': body.length
-      }
-    }
+    const options = optionFactory(urlObject, apiKey, {
+      'Content-Type': 'application/json',
+      'Content-Length': body.length
+    })
 
     const req = request
       .request(options, res => {
@@ -47,7 +57,45 @@ export const post = async <T>(
     req.end()
   })
 
-export const upload = async <T>(): Promise<T> =>
+export const upload = async <T>(
+  url: string,
+  apiKey: string,
+  path: string
+): Promise<T> =>
   new Promise((resolve, reject) => {
-    reject(new Error('Not implemented'))
+    const readStream = createReadStream(path)
+
+    const form = new FormData()
+    form.append('artefact', readStream)
+
+    const urlObject = new URL(url)
+
+    const request = urlObject.protocol === 'https:' ? https : http
+
+    const options = optionFactory(urlObject, apiKey, {
+      'Content-Length': form.getLengthSync(),
+      'Content-Type': form.getHeaders()['content-type']
+    })
+
+    const req = request
+      .request(options, res => {
+        let r = ''
+
+        res.on('data', chunk => {
+          r += chunk
+        })
+
+        res.on('end', () => {
+          if (res.statusCode !== 200 && res.statusCode !== 201) {
+            reject(new Error(`Status code: ${res.statusCode} - ${r}`))
+          } else {
+            resolve(JSON.parse(r))
+          }
+        })
+      })
+      .on('error', err => {
+        reject(err)
+      })
+
+    req.end()
   })
